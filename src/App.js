4a56d8c6c4bfdad84e090bd31eadbe380fefb193
Daniele Galluccio <daniele.galluccio@gmail.com>
2019-11-29 13:45:15 +0000

    edit app file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacting fast
+          I was Learn Reacting fast
         </a>
       </header>
     </div>
